2f03592e0ee3dce00787998a1eb9d3a90d4bcc74
L3XxXa <alexkkk@inbox.ru>
2022-05-07 13:10:07 +0700

    Finally done everything I wanted to implement

--- LearningCaching/app/src/main/java/com/example/learningcaching/MainActivity.kt
+++ LearningCaching/app/src/main/java/com/example/learningcaching/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.learningcaching
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import java.io.File
+import java.nio.file.Files
 
 class MainActivity : AppCompatActivity() {
     private val filename = "superfile"
@@ -14,7 +17,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cachedText : TextView
     private lateinit var cacheButton : Button
     private lateinit var getCacheButton: Button
+    private lateinit var clearCacheButton: Button
+    private lateinit var getCacheSizeButton: Button
+    private lateinit var cacheSizeTextView: TextView
     private lateinit var cacheFile : File
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,19 +37,45 @@ class MainActivity : AppCompatActivity() {
         getCacheButton.setOnClickListener {
             getCache()
         }
+        clearCacheButton = findViewById(R.id.clearCacheButton)
+        clearCacheButton.setOnClickListener {
+            clearCache()
+        }
+        getCacheSizeButton = findViewById(R.id.getCacheSize)
+        getCacheSizeButton.setOnClickListener {
+            getCacheSize()
+        }
+        cacheSizeTextView = findViewById(R.id.cacheSize)
+    }
+
+    private fun getCacheSize() {
+        val fileLength = cacheFile.length()
+        cacheSizeTextView.text = "Cache size: $fileLength"
+    }
+
+    private fun clearCache() {
+        cacheFile.delete()
     }
 
     private fun cacheData() {
-        cacheFile.writeText(textToCache.text.toString())
+        cacheFile.appendText(textToCache.text.toString() + "\n")
         Toast.makeText(applicationContext, "Successfully cached data", Toast.LENGTH_SHORT).show()
     }
 
     private fun getCache() {
+
+        try {
         val textValue = java.lang.StringBuilder()
         cacheFile.forEachLine {
             textValue.append(it)
+            textValue.append("\n")
         }
         cachedText.text = textValue.toString()
+        } catch (e: Exception){
+            Toast.makeText(this, "Cache is empty", Toast.LENGTH_SHORT).show()
+            cacheFile = File(applicationContext.cacheDir, filename)
+
+        }
     }
 
 
